ad8a58f7b2b9f484c9bdcb11d88649e17949b68b
StanaAndrei <stadey33@gmail.com>
2022-03-05 09:40:10 +0200

    added human gif

--- globalVars.js
+++ globalVars.js
@@ -10,7 +10,7 @@ export default {
     MONSTERS_UPDATE_INT: 55,
     //images
     MONSTER_GIF_ADR: './imgs/slime.gif',
-    PLAYER_IMG_ADR: 'https://img.poki.com/cdn-cgi/image/quality=78,width=600,height=600,fit=cover,g=0.5x0.5,f=auto/4b3dc6ed100907c4376302664ee04c85.png',
+    PLAYER_IMG_ADR: './imgs/runner.gif',
     MINER_IMG_ADR: './imgs/miner.jpg',
     MONSTER_GIF: null,
     PLAYER_IMG: null,
